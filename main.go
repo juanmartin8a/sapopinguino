e1a6da312332dfc73d83545ad1fbfdc77c1bc23b
Juan Martin Zabala Ochoa <juanmarzabala8a@gmail.com>
2025-03-22 15:35:33 -0500

    main.go websockets (needs better error handling?)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	aiutils "sapopinguino/internal/ai"
 	awsutils "sapopinguino/internal/aws"
@@ -10,6 +11,7 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/service/apigatewaymanagementapi"
 	"github.com/sashabaranov/go-openai"
 )
 
@@ -23,44 +25,62 @@ func init() {
     dbutils.ConfigDB()
 }
 
-func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    // gpt 4o-mini call
-    // return res to client
-    // store data in DB
+func handler(ctx context.Context, event events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
+    connectionID := event.RequestContext.ConnectionID
 
-    _, err := aiutils.ChatCompletion(ctx, openai.GPT4o, aiutils.SystemRoleContent, `{
+    tokens := []*aiutils.Token{}
+
+    tokenStreamChannel := aiutils.ChatCompletion(ctx, openai.GPT4o, aiutils.SystemRoleContent, `{
         "input_language": "English",
         "target_language": "Spanish",
-        "input": "Hi! Hello my friends. abc, easy as do re mi, or as simple as 123, abc 123 baby you and me girl"
+        "input": "abc, easy as do re mi, or as simple as 123, abc 123 baby you and me girl"
     }`)
+
+    for res := range tokenStreamChannel {
+		if res.Error != nil {
+			log.Printf("\nError encountered: %v\n", res.Error)
+            _, err := awsutils.APIGatewayClient.PostToConnection(ctx, &apigatewaymanagementapi.PostToConnectionInput{
+                ConnectionId: &connectionID,
+                Data:         []byte("<error:/>"),
+            })
+            if err != nil {
+                log.Println("Error sending error token to client: %s", err)
+            }
+			break
+		}
+
+        log.Println(res.Response.Type)
+
+        tokens = append(tokens, res.Response)
+
+        jsonData, err := json.Marshal(res.Response)
+	    if err != nil {
+		    log.Println("Error marshaling JSON:", err)
+            break
+	    }
+
+        _, err = awsutils.APIGatewayClient.PostToConnection(ctx, &apigatewaymanagementapi.PostToConnectionInput{
+			ConnectionId: &connectionID,
+			Data:         jsonData,
+		})
+        if err != nil {
+		    log.Println("Error sending token to client: %s", err)
+            break
+        }
+	}
+    _, err := awsutils.APIGatewayClient.PostToConnection(ctx, &apigatewaymanagementapi.PostToConnectionInput{
+        ConnectionId: &connectionID,
+        Data:         []byte("<end:)>"),
+    })
     if err != nil {
-        log.Println(err)
+        log.Println("Error sending <end:)> thingy to client: %s", err)
     }
-    // log.Println(*res)
-
-    // db
-    // separate input by words,
-    // separate output by words
-    // merge input and output into a map
-    // 
-
-    // for i, token := range res.Tokens {
-    //     if token.Type == "word" {
-    //          
-    //     } else {
-    //
-    //     }
-    // }
-
-
-
-
-
 
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "\"Hello from Lambda!\"",
 	}
+
 	return response, nil
 }
 
